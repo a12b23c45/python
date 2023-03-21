883c35c2a17a541f7ab07c331a0fc77009d8901b
a12b23c45 <128162211+a12b23c45@users.noreply.github.com>
2023-03-21 16:51:15 +0530

    Add files via upload

---
+++ adding2List.py
@@ -0,0 +1,9 @@
+// adding two list by making new list in python
+
+list1 = ["Tomatoes","Potato","Onion"]
+list2 = ["Apple","Banana","Watermelon"]
+x = list1 + list2
+print(x)
+ 
+
+ >output=['tomatoes','potato','onion','apple','banana','watermelon',]
