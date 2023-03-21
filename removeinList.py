098e128d3aa33ef46317a8fee28c06c133bb3fd6
a12b23c45 <128162211+a12b23c45@users.noreply.github.com>
2023-03-21 16:54:54 +0530

    Add files via upload

---
+++ removeinList.py
@@ -0,0 +1,14 @@
+// removing an element from the lsit by using 'pop' and 'clear' 
+// removing at particular index
+
+
+
+c = ["Sun","Moon","Water","Sand","Air"]
+c.pop( 3 )
+print(c)
+c.clear()
+print(c)
+
+
+//>output
+//sun moon water air
