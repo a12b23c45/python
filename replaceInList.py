fff597304dcf680d2c26d4f99d60de34bea531c6
a12b23c45 <128162211+a12b23c45@users.noreply.github.com>
2023-03-21 16:34:39 +0530

    Add files via upload

---
+++ replaceInList.py
@@ -0,0 +1,15 @@
+/*To change the value of a specific item, you can refer to the index number of the item.
+
+Write a program which does the following:
+
+You are given a list 
+�
+x = ["India","USA","France"]
+Add "Russia" in place of "France" in the list 
+�
+x
+Once the element is replaced, print the complete list.*/
+
+x = ["India","USA","France"]
+x[2] = "Russia"
+print(x)
