5583894910c43d53cc65a9069829b19f1f38fade
a12b23c45 <128162211+a12b23c45@users.noreply.github.com>
2023-03-21 16:48:41 +0530

    Add files via upload

---
+++ descending(list).py
@@ -0,0 +1,9 @@
+//printing list in descending order
+//list_name.sort(reverse=true)// list name can be like a=["1,2,3]// list name is 'a'
+
+a = ["Mountain","Valley","River","Sky"]
+b = [3,7,22,1,43]
+b.sort(reverse = True)
+a.sort(reverse = True)
+print(a)
+print(b)
