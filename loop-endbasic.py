89574c142d44aba94420e77151d44b6579a99e57
a12b23c45 <128162211+a12b23c45@users.noreply.github.com>
2023-03-23 14:19:49 +0530

    Add files via upload

---
+++ loop-endbasic.py
@@ -0,0 +1,11 @@
+//Returns a sequence of numbers, starting from 0, and increments by 1 (by default)
+It ends at one less than the specified number.
+For eg in this range - range(10) the loop will end on 9.
+//loop end one less if 9 then range (10)
+
+
+
+
+for x in range( 25 ):
+  print(x)
+print("LOOP ENDED")
