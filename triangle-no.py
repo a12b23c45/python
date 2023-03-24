b6fa5a09c22825117b5790506c00020845452773
a12b23c45 <128162211+a12b23c45@users.noreply.github.com>
2023-03-24 13:15:00 +0530

    Add files via upload

---
+++ triangle-no.py
@@ -0,0 +1,18 @@
+//triangle formation with numbers in python
+
+
+
+
+rows = int(input())
+for i in range(rows):
+    for j in range(i+1):
+        print(j+1, end="") #end ="" means the output should end with an empty string
+    print("")
+
+
+    //1
+12
+123
+1234
+12345
+123456
